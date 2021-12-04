0a724f73ed2c18f90ef13c9738432e81e6bd2cb9
Jeff_Gore <JefGore@gmail.com>
2021-12-03 19:23:54 -0500

    bar detail page

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ import path from 'path'
 import { fileURLToPath } from 'url'
 import logger from 'morgan'
 import cors from 'cors'
-import tailwindcss from 'tailwindcss'
 
 
 import('./config/database.js')
