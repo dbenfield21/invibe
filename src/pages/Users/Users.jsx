1406e7e63d4f790149dadecc5cfbf5ea2ed3cbe6
Jeff_Gore <JefGore@gmail.com>
2022-01-22 14:19:49 -0500

    test

--- src/pages/Users/Users.jsx
+++ src/pages/Users/Users.jsx
@@ -14,25 +14,25 @@ const Users = (props) => {
 
   return (
     <>
-    <div className={styles.users}>
-    <div className={styles.usersBackground}>
-      <div className={styles.usersCard}>
-      <h1 className={styles.head}>InVibe Users
-    <span role="img" aria-label="clinking cocktail glasses">🥂</span></h1>
+      <div className={styles.users}>
+        <div className={styles.usersBackground}>
+          <div className={styles.usersCard}>
+            <h1 className={styles.head}>InVibe Users
+            <span role="img" aria-label="clinking cocktail glasses">🥂</span></h1>
 
-      {profiles ? 
-      <div className={styles.wrap}>
-        {profiles.map(profile=>
-          <Link to={`/profile/${profile._id}`} state={profile._id}>
-          <p className={styles.usersName} key={profile._id}>{profile.name}</p></Link>
-        )}
+          {profiles ? 
+            <div className={styles.wrap}>
+              {profiles.map(profile=>
+                <Link to={`/profile/${profile._id}`} state={profile._id}>
+                <p className={styles.usersName} key={profile._id}>{profile.name}</p></Link>
+              )}
+            </div>
+          :
+            <p>An error occurred</p>
+          }
+        </div>
+        </div>  
       </div>
-      :
-        <p>An error occurred</p>
-      }
-    </div>
-    </div>  
-    </div>
     </>
   );
 }
